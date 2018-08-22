4d0f34e13e1640ee68d86fdec454c4c0f27a762e
Jakob Nordlander <wonymn@hotmail.com>
2018-08-22 21:58:04 +0100

    Hit 1 pin

--- BowlingKata/Attempt1/BowlingKata/Game.cs
+++ BowlingKata/Attempt1/BowlingKata/Game.cs
@@ -2,9 +2,21 @@ namespace BowlingKata
 {
     public class Game
     {
+        private int _score;
+
+        public Game()
+        {
+            _score = 0;
+        }
+
         public int Score()
         {
-            return 0;
+            return _score;
+        }
+
+        public void Roll(int numberOfPinsKnockedDown)
+        {
+            _score += numberOfPinsKnockedDown;
         }
     }
 }
--- BowlingKata/Attempt1/BowlingKata/GameTests.cs
+++ BowlingKata/Attempt1/BowlingKata/GameTests.cs
@@ -12,5 +12,15 @@ namespace BowlingKata
 
             Assert.Equal(0, game.Score());
         }
+
+        [Fact]
+        public void RollAndHitOnePin()
+        {
+            var game = new Game();
+
+            game.Roll(1);
+
+            Assert.Equal(1, game.Score());
+        }
     }
 }
