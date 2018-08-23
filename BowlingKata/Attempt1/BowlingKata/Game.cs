a1c6d71b05c1f8cfde2947a7d4c546e0e18bd7d2
Jakob Nordlander <wonymn@hotmail.com>
2018-08-23 21:48:41 +0100

    Refactoring

--- BowlingKata/Attempt1/BowlingKata/Game.cs
+++ BowlingKata/Attempt1/BowlingKata/Game.cs
@@ -19,25 +19,50 @@ namespace BowlingKata
             for (int i = 0; i < _frames.Count; i++)
             {
                 score += _frames[i].Score();
-                if (_frames[i].WasStrike() || _frames[i].WasSpare())
-                {
-                    if (i < _frames.Count - 1)
-                    {
-                        score += _frames[i + 1].Score();
-                    }
 
-                    if (_frames[i].WasStrike())
-                    {
-                        if (i < _frames.Count - 2)
-                        {
-                            score += _frames[i + 2].Score();
-                        }
-                    }
-                }
+                score += ScoreFromNextFrame(i);
+
+                score += ScoreFromNextNextFrame(i);
             }
 
             return score;
         }
+
+        private int ScoreFromNextNextFrame(int i)
+        {
+            if (AddScoreFromNextNextFrame(i))
+            {
+                return _frames[i + 2].Score();
+            }
+
+            return 0;
+        }
+
+        private int ScoreFromNextFrame(int i)
+        {
+            if (AddScoreFromNextFrame(i))
+            {
+                return _frames[i + 1].Score();
+            }
+
+            return 0;
+        }
+
+        private bool AddScoreFromNextNextFrame(int i)
+        {
+            return _frames[i].WasStrike() && ThereAreEnoughFramesLeft(i, _frames.Count, 2);
+        }
+
+        private bool AddScoreFromNextFrame(int i)
+        {
+            return (_frames[i].WasStrike() || _frames[i].WasSpare()) && ThereAreEnoughFramesLeft(i, _frames.Count, 1);
+        }
+
+        private bool ThereAreEnoughFramesLeft(int currentFrameIndex, int nrOfFrames, int nrOfRequiredFrames)
+        {
+            return currentFrameIndex < nrOfFrames - nrOfRequiredFrames;
+        }
+
 
         public void Roll(int numberOfPinsKnockedDown)
         {
