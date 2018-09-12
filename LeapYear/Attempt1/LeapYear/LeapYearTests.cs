e1bd1b522251b510d0348e98fc1c2903db3bc607
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-09-12 22:16:52 +0100

    Refactored tests

--- LeapYear/Attempt1/LeapYear/LeapYearTests.cs
+++ LeapYear/Attempt1/LeapYear/LeapYearTests.cs
@@ -4,37 +4,20 @@ namespace LeapYear
 
     public class LeapYearTests
     {
-        [Fact]
-        public void NotALeapYear()
-        {
-            Assert.False(IsLeapYear(2017));
-        }
-
         [Theory]
-        [InlineData(2016)]
-        [InlineData(2020)]
-        [InlineData(2024)]
-        public void RegularLeapYear(int regularLeapYear)
+        [InlineData(2017, false)]
+        [InlineData(1000, false)]
+        [InlineData(1600, true)]
+        [InlineData(1800, false)]
+        [InlineData(1900, false)]
+        [InlineData(2000, true)]
+        [InlineData(2016, true)]
+        [InlineData(2020, true)]
+        [InlineData(2024, true)]
+        [InlineData(2400, true)]
+        public void CheckLeapYear(int year, bool expectedValue)
         {
-            Assert.True(IsLeapYear(regularLeapYear));
-        }
-
-        [Theory]
-        [InlineData(1000)]
-        [InlineData(1800)]
-        [InlineData(1900)]
-        public void ATypicalCommonYear(int aTypicalCommonYear)
-        {
-            Assert.False(IsLeapYear(aTypicalCommonYear));
-        }
-
-        [Theory]
-        [InlineData(1600)]
-        [InlineData(2000)]
-        [InlineData(2400)]
-        public void ATypicalLeapYear(int aTypicalLeapYear)
-        {
-            Assert.True(IsLeapYear(aTypicalLeapYear));
+            Assert.Equal(expectedValue, IsLeapYear(year));
         }
 
         private bool IsLeapYear(int year)
