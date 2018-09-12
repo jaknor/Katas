52781092d0fea4d25eb0f7c78bad172a70f73197
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-09-12 21:51:07 +0100

    First test for regular leap year

--- LeapYear/Attempt1/LeapYear/LeapYearTests.cs
+++ LeapYear/Attempt1/LeapYear/LeapYearTests.cs
@@ -11,8 +11,19 @@ namespace LeapYear
             Assert.False(IsLeapYear(2017));
         }
 
+        [Fact]
+        public void RegularLeapYear()
+        {
+            Assert.True(IsLeapYear(2020));
+        }
+
         private bool IsLeapYear(int year)
         {
+            if (year == 2020)
+            {
+                return true;
+            }
+
             return false;
         }
     }
