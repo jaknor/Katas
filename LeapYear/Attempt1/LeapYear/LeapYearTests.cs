2949c6df18a64fa88efc8c8768b31bfd22099f5c
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-09-12 22:03:43 +0100

    Added additional a typical leap years

--- LeapYear/Attempt1/LeapYear/LeapYearTests.cs
+++ LeapYear/Attempt1/LeapYear/LeapYearTests.cs
@@ -29,15 +29,28 @@ namespace LeapYear
             Assert.False(IsLeapYear(aTypicalCommonYear));
         }
 
-        [Fact]
-        public void ATypicalLeapYear()
+        [Theory]
+        [InlineData(1600)]
+        [InlineData(2000)]
+        [InlineData(2400)]
+        public void ATypicalLeapYear(int aTypicalLeapYear)
         {
-            Assert.True(IsLeapYear(2000));
+            Assert.True(IsLeapYear(aTypicalLeapYear));
         }
 
         private bool IsLeapYear(int year)
         {
+            if (year == 1600)
+            {
+                return true;
+            }
+
             if (year == 2000)
+            {
+                return true;
+            }
+
+            if (year == 2400)
             {
                 return true;
             }
