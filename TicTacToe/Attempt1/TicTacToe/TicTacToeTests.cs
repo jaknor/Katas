87aa63603203e54803f69290b09c5562f5eaea9f
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-09-30 22:09:50 +0100

    Third test

--- TicTacToe/Attempt1/TicTacToe/TicTacToeTests.cs
+++ TicTacToe/Attempt1/TicTacToe/TicTacToeTests.cs
@@ -24,5 +24,16 @@ namespace TicTacToe
 
             Assert.Equal(board.ToString(), "XEEEEEEEE");
         }
+
+        [Fact]
+        public void CannotPlacePieceOnTopOfOtherPiece()
+        {
+            var board = new Board();
+
+            board.Place(new Piece("X"));
+            board.Place(new Piece("O"));
+
+            Assert.Equal(board.ToString(), "XEEEEEEEE");
+        }
     }
 }
