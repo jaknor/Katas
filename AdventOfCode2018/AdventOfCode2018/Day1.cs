151d50ce40be2962a032c6c42443f68e2c263ed7
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-12-12 22:45:51 +0000

    Day 1 - First test

---
+++ AdventOfCode2018/AdventOfCode2018/Day1.cs
@@ -0,0 +1,37 @@
+namespace AdventOfCode2018
+{
+    using Shouldly;
+    using TestStack.BDDfy;
+    using Xunit;
+
+    public class Day1
+    {
+        private int _frequency;
+        private string _input;
+
+        [Fact]
+        public void OnePositiveNumber()
+        {
+            this
+                .Given(_ => GivenInput("+1"))
+                .When(_ => WhenWeCalculateFrequency())
+                .Then(_ => ThenTheCalculatedFrequencyIs(1))
+                .BDDfy();
+        }
+
+        private void GivenInput(string input)
+        {
+            _input = input;
+        }
+
+        private void WhenWeCalculateFrequency()
+        {
+            _frequency = new FrequencyCalculator().Calculate(_input);
+        }
+
+        private void ThenTheCalculatedFrequencyIs(int expectedFrequency)
+        {
+            _frequency.ShouldBe(expectedFrequency);
+        }
+    }
+}
---
+++ AdventOfCode2018/AdventOfCode2018/FrequencyCalculator.cs
@@ -0,0 +1,10 @@
+namespace AdventOfCode2018
+{
+    internal class FrequencyCalculator
+    {
+        public int Calculate(string input)
+        {
+            return 1;
+        }
+    }
+}
