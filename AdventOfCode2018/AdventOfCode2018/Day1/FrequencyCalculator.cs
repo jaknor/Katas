e7703a447da281840edba48d6c0b5210e57143a7
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-12-12 23:21:36 +0000

    Day 1 - Part 2 - Complete solution

--- AdventOfCode2018/AdventOfCode2018/Day1/FrequencyCalculator.cs
+++ AdventOfCode2018/AdventOfCode2018/Day1/FrequencyCalculator.cs
@@ -1,5 +1,7 @@
 namespace AdventOfCode2018.Day1
 {
+    using System.Collections.Generic;
+
     internal class FrequencyCalculator
     {
         public int Calculate(string input)
@@ -25,7 +27,30 @@ namespace AdventOfCode2018.Day1
 
         public int FindFirstFrequencyToBeRepeated(string input)
         {
-            return 0;
+            var frequencies = new HashSet<int>() {0};
+            var frequency = 0;
+            var values = input.Split("|");
+
+            while (true)
+            {
+                foreach (var value in values)
+                {
+                    var sign = value.Substring(0, 1);
+                    var frequencyChange = int.Parse(value.Substring(1));
+
+                    if (sign == "-")
+                    {
+                        frequencyChange = -frequencyChange;
+                    }
+
+                    frequency += frequencyChange;
+                    if (frequencies.Contains(frequency))
+                    {
+                        return frequency;
+                    }
+                    frequencies.Add(frequency);
+                }
+            }
         }
     }
 }
