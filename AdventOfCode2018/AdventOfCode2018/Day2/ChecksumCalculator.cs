2e6e53bcecd2e8e44cd7df968ed30109d751de91
Jakob Nordlander <jakob.nordlander@gmail.com>
2018-12-13 22:30:30 +0000

    Day 2 - Part 1 - Test for empty input

---
+++ AdventOfCode2018/AdventOfCode2018/Day2/ChecksumCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2018.Day2
+{
+    internal class ChecksumCalculator
+    {
+        public int Calculate(string input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2018/AdventOfCode2018/Day2/Part1.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2018.Day2
+{
+    using Shouldly;
+    using TestStack.BDDfy;
+    using Xunit;
+
+    public class Part1
+    {
+        private string _input;
+        private int _result;
+
+        [Fact]
+        public void EmptyInput()
+        {
+            this
+                .Given(_ => GivenInput(""))
+                .When(_ => WhenWeCalculateChecksum())
+                .Then(_ => ThenTheChecksumIs(0))
+                .BDDfy();
+        }
+
+        private void GivenInput(string input)
+        {
+            _input = input;
+        }
+
+        private void WhenWeCalculateChecksum()
+        {
+            _result = new ChecksumCalculator().Calculate(_input);
+        }
+
+        private void ThenTheChecksumIs(int expectedChecksum)
+        {
+            _result.ShouldBe(expectedChecksum);
+        }
+    }
+}
