01e6a864decf048e00cbf82dc6f008a3c9e45962
Jakob Nordlander <jakob.nordlander@gmail.com>
2019-03-16 16:02:41 +0000

    Refactored test

--- MarsRover/Attempt 1/MarsRover/MarsRover/RoverTests.cs
+++ MarsRover/Attempt 1/MarsRover/MarsRover/RoverTests.cs
@@ -29,23 +29,28 @@ namespace MarsRover
             new List<object[]>
             {
                 // Move forward north 1 step
-                new object[] { new RoverState(0, 0, "N"),  new List<string> { "f" }, new RoverState(0, 1, "N")   },
+                new object[] { RoverInMiddleFacing("N"),  new List<string> { "f" }, new RoverState(0, 1, "N")   },
                 // Move forward north 2 steps
-                new object[] { new RoverState(0, 0, "N"),  new List<string> { "f", "f" }, new RoverState(0, 2, "N")   },
+                new object[] { RoverInMiddleFacing("N"),  new List<string> { "f", "f" }, new RoverState(0, 2, "N")   },
                 // Move forward north 3 steps
-                new object[] { new RoverState(0, 0, "N"),  new List<string> { "f", "f", "f" }, new RoverState(0, 3, "N")   },
+                new object[] { RoverInMiddleFacing( "N"),  new List<string> { "f", "f", "f" }, new RoverState(0, 3, "N")   },
                 // Move forward east 1 step
-                new object[] { new RoverState(0, 0, "E"),  new List<string> { "f" }, new RoverState(1, 0, "E")   },
+                new object[] { RoverInMiddleFacing( "E"),  new List<string> { "f" }, new RoverState(1, 0, "E")   },
                 // Move forward east 2 steps
-                new object[] { new RoverState(0, 0, "E"),  new List<string> { "f", "f" }, new RoverState(2, 0, "E")   },
+                new object[] { RoverInMiddleFacing( "E"),  new List<string> { "f", "f" }, new RoverState(2, 0, "E")   },
                 // Move forward east 3 steps
-                new object[] { new RoverState(0, 0, "E"),  new List<string> { "f", "f", "f" }, new RoverState(3, 0, "E")   },
+                new object[] { RoverInMiddleFacing( "E"),  new List<string> { "f", "f", "f" }, new RoverState(3, 0, "E")   },
                 // Move forward south 1 step
-                new object[] { new RoverState(0, 0, "S"),  new List<string> { "f" }, new RoverState(0, -1, "S")   },
+                new object[] { RoverInMiddleFacing( "S"),  new List<string> { "f" }, new RoverState(0, -1, "S")   },
                 // Move forward south 2 steps
-                new object[] { new RoverState(0, 0, "S"),  new List<string> { "f", "f" }, new RoverState(0, -2, "S")   },
+                new object[] { RoverInMiddleFacing( "S"),  new List<string> { "f", "f" }, new RoverState(0, -2, "S")   },
                 // Move forward south 3 steps
-                new object[] { new RoverState(0, 0, "S"),  new List<string> { "f", "f", "f" }, new RoverState(0, -3, "S")   },
+                new object[] { RoverInMiddleFacing( "S"),  new List<string> { "f", "f", "f" }, new RoverState(0, -3, "S")   },
             };
+
+        private static RoverState RoverInMiddleFacing(string direction)
+        {
+            return new RoverState(0, 0, direction);
+        }
     }
 }
